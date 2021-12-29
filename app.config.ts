1a885e44646110b349a6e44d43c61bd90209346a
Raí Adriano <raivieiraadriano92@gmail.com>
2021-12-29 15:46:15 -0300

    added typescript support for config files

---
+++ app.config.ts
@@ -0,0 +1,32 @@
+import { ExpoConfig } from "@expo/config-types";
+
+const config: ExpoConfig = {
+  name: "expo-shopper-ecommerce-ui-kit",
+  slug: "expo-shopper-ecommerce-ui-kit",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  splash: {
+    image: "./assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ["**/*"],
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#FFFFFF",
+    },
+  },
+  web: {
+    favicon: "./assets/favicon.png",
+  },
+};
+
+export default config;
