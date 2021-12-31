7c2be55c3f0fd79df8a241c05fb4416195825440
Raí Adriano <raivieiraadriano92@gmail.com>
2021-12-31 11:13:35 -0300

    test: temporarily added ts-ignore in TabStackNavigator.test

--- src/navigation/TabStackNavigator.test.tsx
+++ src/navigation/TabStackNavigator.test.tsx
@@ -4,6 +4,7 @@ import { TabStackNavigator } from './TabStackNavigator'
 
 describe('<TabStackNavigator />', () => {
   it('renders correctly', async () => {
+    //@ts-ignore
     const { findByTestId, toJSON } = render(<TabStackNavigator />)
 
     await waitFor(() => findByTestId('AppWrapper'))
