a63030ee55cd49ca940d3c39befb688086071dfb
Raí Adriano <raivieiraadriano92@gmail.com>
2021-12-31 21:46:48 -0300

    refactor(appwrapper): added expo-google-fonts package to load poppins font

--- src/AppWrapper.tsx
+++ src/AppWrapper.tsx
@@ -1,66 +1,50 @@
-import { FunctionComponent, useCallback, useState } from 'react'
+import { FunctionComponent } from 'react'
 
+import {
+  useFonts,
+  Poppins_900Black,
+  Poppins_700Bold,
+  Poppins_800ExtraBold,
+  Poppins_200ExtraLight,
+  Poppins_300Light,
+  Poppins_500Medium,
+  Poppins_400Regular,
+  Poppins_600SemiBold,
+  Poppins_100Thin
+} from '@expo-google-fonts/poppins'
 import { NavigationContainer } from '@react-navigation/native'
 import AppLoading from 'expo-app-loading'
-import * as Font from 'expo-font'
 import { NativeBaseProvider } from 'native-base'
-import { View } from 'react-native'
 
-import Poppins_900Black from '../assets/fonts/Poppins-Black.ttf'
-import Poppins_700Bold from '../assets/fonts/Poppins-Bold.ttf'
-import Poppins_800ExtraBold from '../assets/fonts/Poppins-ExtraBold.ttf'
-import Poppins_200ExtraLight from '../assets/fonts/Poppins-ExtraLight.ttf'
-import Poppins_300Light from '../assets/fonts/Poppins-Light.ttf'
-import Poppins_500Medium from '../assets/fonts/Poppins-Medium.ttf'
-import Poppins_400Regular from '../assets/fonts/Poppins-Regular.ttf'
-import Poppins_600SemiBold from '../assets/fonts/Poppins-SemiBold.ttf'
-import Poppins_100Thin from '../assets/fonts/Poppins-Thin.ttf'
 import { theme } from './theme'
 
 export const AppWrapper: FunctionComponent = ({ children }) => {
-  const [isReady, setIsReady] = useState(false)
+  const [fontsLoaded] = useFonts({
+    Poppins_900Black,
+    Poppins_700Bold,
+    Poppins_800ExtraBold,
+    Poppins_200ExtraLight,
+    Poppins_300Light,
+    Poppins_500Medium,
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+    Poppins_100Thin
+  })
 
-  const loadFonts = useCallback(async () => {
-    try {
-      await Font.loadAsync({
-        Poppins_100Thin,
-        Poppins_200ExtraLight,
-        Poppins_300Light,
-        Poppins_400Regular,
-        Poppins_500Medium,
-        Poppins_600SemiBold,
-        Poppins_700Bold,
-        Poppins_800ExtraBold,
-        Poppins_900Black
-      })
-    } catch (err) {}
-  }, [])
-
-  const loadResources = useCallback(async () => {
-    await loadFonts()
-    // other resources ...
-  }, [loadFonts])
-
-  if (!isReady) {
-    return (
-      <AppLoading
-        startAsync={loadResources}
-        onFinish={() => setIsReady(true)}
-        onError={console.warn}
-      />
-    )
-  }
-
-  return (
-    <View style={{ flex: 1 }} testID="AppWrapper">
-      <NativeBaseProvider
-        config={{
-          enableRem: true,
-          suppressColorAccessibilityWarning: true
-        }}
-        {...{ theme }}>
-        <NavigationContainer>{children}</NavigationContainer>
-      </NativeBaseProvider>
-    </View>
+  return !fontsLoaded ? (
+    <AppLoading />
+  ) : (
+    <NativeBaseProvider
+      config={{
+        enableRem: true,
+        suppressColorAccessibilityWarning: true
+      }}
+      initialWindowMetrics={{
+        frame: { x: 0, y: 0, width: 0, height: 0 },
+        insets: { top: 0, left: 0, right: 0, bottom: 0 }
+      }}
+      {...{ theme }}>
+      <NavigationContainer>{children}</NavigationContainer>
+    </NativeBaseProvider>
   )
 }
